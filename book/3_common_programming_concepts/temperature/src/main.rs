eed94e05755d32038b653f888fa9c94a1ce8803e
Nicky Meuleman <nicky.dev@outlook.com>
2020-09-17 23:17:46 +0200

    temperature exercise fix

--- book/3_common_programming_concepts/temperature/src/main.rs
+++ book/3_common_programming_concepts/temperature/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let num: f64 = args[1].parse().expect("First argument must be a number");
-    let scale: String = args[2]
-        .parse()
-        .expect("Second arg must be c for Celsius, or f for Fahrenheit.");
+    let scale: String = args[2].parse().expect("Second argument must be a string.");
     let is_c: Option<bool> = if scale == "C" {
         Some(true)
     } else if scale == "F" {
         Some(false)
     } else {
-        None
+        panic!("Second arg must be C for Celsius, or F for Fahrenheit.");
     };
     let result = if is_c.unwrap() {
         c_to_f(num)
@@ -26,7 +24,7 @@ fn main() {
 }
 
 fn f_to_c(f: f64) -> f64 {
-    f - 32.0 * (5.0 / 9.0)
+    (f - 32.0) * (5.0 / 9.0)
 }
 
 fn c_to_f(c: f64) -> f64 {
