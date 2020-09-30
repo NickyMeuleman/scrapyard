93b1359c5e568e77c6e607285c7226040f139fae
Nicky Meuleman <nicky.dev@outlook.com>
2020-09-30 21:07:40 +0200

    experiment with matching on struct vs on enum

--- book/playground/src/main.rs
+++ book/playground/src/main.rs
@@ -1,5 +1,42 @@
 fn main() {
+    // START
+
     const SPEED_LIMIT: u32 = 299_792_458;
-    println!("{}",SPEED_LIMIT);
+    println!("{}", SPEED_LIMIT);
     // 299792458
+    struct Action<'a> {
+        a_type: &'a str,
+    }
+    let struct_instance = Action { a_type: "ADD" };
+
+    // END - START
+
+    match struct_instance {
+        Action { a_type: "ADD" } => {
+            println!("struct: found ADD");
+        }
+        // have to define a catchall case since the struct has infinite possibilities
+        Action { a_type: _ } => {
+            println!("struct: catchall case");
+        }
+    }
+
+    enum AType {
+        ADD,
+        REMOVE,
+    }
+
+    let enum_variant = AType::ADD;
+
+    match enum_variant {
+        // don't have to provide a catchall case, as long as we cover every possible value for the enum
+        AType::ADD => {
+            println!("enum: found ADD");
+        }
+        AType::REMOVE => {
+            println!("enum: found REMOVE");
+        }
+    }
+
+    // END
 }
