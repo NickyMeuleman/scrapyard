ee4d97f302a563ce277a1d63d7f666f2c521f246
Nicky Meuleman <nicky.dev@outlook.com>
2020-09-30 20:01:06 +0200

    rustlings modules

--- rustlings/exercises/modules/modules2.rs
+++ rustlings/exercises/modules/modules2.rs
@@ -1,18 +1,38 @@
 // modules2.rs
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
+// this works:
+// mod delicious_snacks {
+//     pub use self::fruits::PEAR as fruit;
+//     pub use self::veggies::CUCUMBER as veggie;
 
+//     mod fruits {
+//         pub const PEAR: &'static str = "Pear";
+//         pub const APPLE: &'static str = "Apple";
+//     }
+
+//     mod veggies {
+//         pub const CUCUMBER: &'static str = "Cucumber";
+//         pub const CARROT: &'static str = "Carrot";
+//     }
+// }
+
+// fn main() {
+//     println!(
+//         "favorite snacks: {} and {}",
+//         delicious_snacks::fruit,
+//         delicious_snacks::veggie
+//     );
+// }
+
+// alternative solution by not re-exporting from within delicious_snacks
 mod delicious_snacks {
-    use self::fruits::PEAR as fruit;
-    use self::veggies::CUCUMBER as veggie;
-
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -21,7 +41,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        delicious_snacks::fruits::PEAR,
+        delicious_snacks::veggies::CUCUMBER
     );
 }
