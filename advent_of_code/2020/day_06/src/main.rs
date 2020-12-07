e87a98460aed0c84a10b92ac04431acd223da630
Nicky Meuleman <nicky.dev@outlook.com>
2020-12-07 15:42:29 +0100

    refactor: aoc2020 day06 to use hashset union and intersection

--- advent_of_code/2020/day_06/src/main.rs
+++ advent_of_code/2020/day_06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
@@ -8,47 +8,48 @@ fn main() {
     println!("part two answer: {}", part_two(&answers));
 }
 
-fn parse(input: &String) -> Vec<(usize, HashMap<char, usize>)> {
+fn parse(input: &String) -> Vec<Vec<HashSet<char>>> {
     input
         .split("\n\n")
         .map(|block| parse_block(block))
         .collect()
 }
 
-fn parse_block(block: &str) -> (usize, HashMap<char, usize>) {
-    let block: Vec<HashSet<char>> = block.split("\n").map(|line| parse_line(line)).collect();
-    let mut map: HashMap<char, usize> = HashMap::new();
-    let mut num_lines = 0;
-    for line in block {
-        for c in line {
-            map.entry(c).and_modify(|val| *val += 1).or_insert(1);
-        }
-        num_lines += 1;
-    }
-    (num_lines, map)
+fn parse_block(block: &str) -> Vec<HashSet<char>> {
+    block.split("\n").map(|line| parse_line(line)).collect()
 }
 
 fn parse_line(line: &str) -> HashSet<char> {
     line.chars().collect()
 }
 
-fn part_one(answers: &Vec<(usize, HashMap<char, usize>)>) -> usize {
-    answers.iter().map(|block| block.1.len()).sum()
+fn part_one(answers: &Vec<Vec<HashSet<char>>>) -> usize {
+    answers
+        .into_iter()
+        .map(|block| {
+            let start_acc = HashSet::new();
+            block
+                .into_iter()
+                .fold(start_acc, |acc, line| {
+                    acc.union(line).map(|c| *c).collect()
+                })
+                .len()
+        })
+        .sum()
 }
 
-fn part_two(answers: &Vec<(usize, HashMap<char, usize>)>) -> usize {
-    answers.iter().map(|block| get_every(block.0, &block.1)).sum()
-}
-
-fn get_every(line_num: usize, map: &HashMap<char, usize>) -> usize {
-    let mut count = 0;
-    for (_, v) in map {
-        if *v == line_num {
-            count += 1
-        }
-    }
-    dbg!(line_num,&map);
-    count
+fn part_two(answers: &Vec<Vec<HashSet<char>>>) -> usize {
+    answers
+        .into_iter()
+        .map(|block| {
+            // Rust has an experimental fold_first method where the first item is the starting value of the acc
+            let start_acc = block[0].clone();
+            block
+                .into_iter()
+                .fold(start_acc, |acc, line| acc.intersection(line).map(|c| *c).collect())
+                .len()
+        })
+        .sum()
 }
 
 #[test]
@@ -71,7 +72,7 @@ b"#
     .to_owned();
     let answers = parse(&input);
     // why is this failing when the puzzle passed with the big input in input.txt? Is the test input wrong?
-    // edit: it was formatting, used the r#"a string whit preserved whitespace"# syntax
+    // edit: it was formatting, used the r#"a string with preserved whitespace"# syntax
     assert_eq!(part_one(&answers), 11);
 }
 
@@ -94,6 +95,5 @@ a
 b"#
     .to_owned();
     let answers = parse(&input);
-    // why is this failing when the puzzle passed with the big input in input.txt? Is the test input wrong?
     assert_eq!(part_two(&answers), 6);
 }
