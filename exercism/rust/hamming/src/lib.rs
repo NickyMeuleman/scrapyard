e72b7e081ca8acf70d3afbd0714594807a6b8c1c
Nicky Meuleman <nicky.dev@outlook.com>
2021-07-04 22:03:50 +0200

    style: format hamming

--- exercism/rust/hamming/src/lib.rs
+++ exercism/rust/hamming/src/lib.rs
@@ -4,7 +4,12 @@ use std::cmp::Ordering;
 
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
     match s1.len().cmp(&s2.len()) {
-        Ordering::Equal => Some(s1.chars().zip(s2.chars()).filter(|(c1, c2)| c1 != c2).count()),
-        _ => None
+        Ordering::Equal => Some(
+            s1.chars()
+                .zip(s2.chars())
+                .filter(|(c1, c2)| c1 != c2)
+                .count(),
+        ),
+        _ => None,
     }
 }
