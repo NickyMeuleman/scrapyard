eca436c572dd453463c8789c9efdf5f620eae3c2
Nicky Meuleman <nicky.dev@outlook.com>
2021-07-04 22:02:33 +0200

    exercism: hamming

---
+++ exercism/rust/hamming/src/lib.rs
@@ -0,0 +1,10 @@
+/// Return the Hamming distance between the strings,
+/// or None if the lengths are mismatched.
+use std::cmp::Ordering;
+
+pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
+    match s1.len().cmp(&s2.len()) {
+        Ordering::Equal => Some(s1.chars().zip(s2.chars()).filter(|(c1, c2)| c1 != c2).count()),
+        _ => None
+    }
+}
