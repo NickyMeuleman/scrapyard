6e7c310c64bfb776e6abdd304a04bb5ae9faf422
Nicky Meuleman <nicky.dev@outlook.com>
2021-07-10 22:15:54 +0200

    exercism: robot-simulator

---
+++ exercism/rust/robot-simulator/src/lib.rs
@@ -0,0 +1,72 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(PartialEq, Debug)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+pub struct Robot {
+    x: i32,
+    y: i32,
+    d: Direction,
+}
+
+impl Robot {
+    pub fn new(x: i32, y: i32, d: Direction) -> Self {
+        Self { x, y, d }
+    }
+
+    pub fn turn_right(self) -> Self {
+        let d = match self.d {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        };
+        Self { d, ..self }
+    }
+
+    pub fn turn_left(self) -> Self {
+        let d = match self.d {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        };
+        Self { d, ..self }
+    }
+
+    pub fn advance(self) -> Self {
+        let (x, y) = match self.d {
+            Direction::North => (self.x, self.y + 1),
+            Direction::East => (self.x + 1, self.y),
+            Direction::South => (self.x, self.y - 1),
+            Direction::West => (self.x - 1, self.y),
+        };
+        Self { x, y, ..self }
+    }
+
+    pub fn instructions(mut self, instructions: &str) -> Self {
+        for c in instructions.chars() {
+            self = match c {
+                'A' => self.advance(),
+                'L' => self.turn_left(),
+                'R' => self.turn_right(),
+                _ => unreachable!(),
+            }
+        }
+        self
+    }
+
+    pub fn position(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    pub fn direction(&self) -> &Direction {
+        &self.d
+    }
+}
