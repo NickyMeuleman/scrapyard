d864016682055f933a25df1548e5e400193c4723
Nicky Meuleman <nicky.dev@outlook.com>
2021-08-02 01:16:29 +0200

    exercism: minesweeper

---
+++ exercism/rust/minesweeper/src/lib.rs
@@ -0,0 +1,36 @@
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    minefield
+        .iter()
+        .enumerate()
+        .map(|(row_idx, row)| {
+            row.chars()
+                .enumerate()
+                .map(|(col_idx, c)| match c {
+                    '*' => '*'.to_string(),
+                    _ => match count_neighbours(minefield, row_idx, col_idx) {
+                        0 => ' '.to_string(),
+                        count => count.to_string(),
+                    },
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn count_neighbours(input: &[&str], row: usize, col: usize) -> usize {
+    input
+        .iter()
+        // take one more than row, remember row is 0 indexed
+        .take(row + 2)
+        // skip one less than row, ensure valid number
+        .skip((row as isize - 1).max(0) as usize)
+        // end up with at most 3 rows, 1 before idx to 1 after idx
+        // now do the same for cols to end up with at most a 3 by 3 section
+        .flat_map(|line| {
+            line.chars()
+                .take(col + 2)
+                .skip((col as isize - 1).max(0) as usize)
+        })
+        .filter(|&c| c == '*')
+        .count()
+}
