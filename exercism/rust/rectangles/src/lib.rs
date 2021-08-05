4f45d2da4674ca2c83c9268cdde67b8a3e0ef4cc
Nicky Meuleman <nicky.dev@outlook.com>
2021-08-06 00:14:11 +0200

    refactor: exercism: rectangles

--- exercism/rust/rectangles/src/lib.rs
+++ exercism/rust/rectangles/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(option_result_contains)]
+
 pub fn count(input: &[&str]) -> usize {
     // loop through all corner (row, col) indexes
     // treat them as the top_left corner of potential rectangles
@@ -21,73 +23,52 @@ pub fn count(input: &[&str]) -> usize {
                     (1..)
                         .take_while(move |i| i + row < rows)
                         .filter(move |&row_offset| {
-                            has_corners(input, row, col, col_offset, row_offset).is_some()
-                                && has_edges(input, row, col, col_offset, row_offset).is_some()
+                            let top_left = (row, col);
+                            let top_right = (row, col + col_offset);
+                            let bottom_left = (row + row_offset, col);
+                            let bottom_right = (row + row_offset, col + col_offset);
+                            let corners = [top_left, top_right, bottom_left, bottom_right];
+                            has_corners(input, corners) && has_edges(input, corners)
                         })
                 })
         })
         .count()
 }
 
-fn has_corners(
-    input: &[&str],
-    row: usize,
-    col: usize,
-    col_offset: usize,
-    row_offset: usize,
-) -> Option<()> {
-    // returning an Option to get early returns, how do you ideomatically do this with booleans?
-    let top_right = input[row].chars().nth(col + col_offset)?;
-    let bottom_left = input[row + row_offset].chars().nth(col)?;
-    let bottom_right = input[row + row_offset].chars().nth(col + col_offset)?;
-
-    (top_right == '+' && bottom_left == '+' && bottom_right == '+').then(|| ())
+fn has_corners(input: &[&str], corners: [(usize, usize); 4]) -> bool {
+    corners
+        .iter()
+        .all(|&(row, col)| input[row].chars().nth(col).contains(&'+'))
 }
 
-fn has_edges(
-    input: &[&str],
-    row: usize,
-    col: usize,
-    col_offset: usize,
-    row_offset: usize,
-) -> Option<()> {
-    // returning an Option to get early returns, how do you ideomatically do this with booleans?
+fn has_edges(input: &[&str], corners: [(usize, usize); 4]) -> bool {
+    // first line is between top_left and top_right, second is between bottom_left and bottom_right
+    let horizontal_points = [(corners[0], corners[1]), (corners[2], corners[3])];
+    // first line is between top_left and bottom_left, second is between top_right and bottom_right
+    let vertical_points = [(corners[0], corners[2]), (corners[1], corners[3])];
 
-    // top
-    input[row]
-        .chars()
-        .skip(col)
-        .take(col_offset + 1)
-        .all(|c| c == '+' || c == '-')
-        .then(|| ())?;
-
-    // bottom
-    input[row + row_offset]
-        .chars()
-        .skip(col)
-        .take(col_offset + 1)
-        .all(|c| c == '+' || c == '-')
-        .then(|| ())?;
-
-    // left
-    input
+    horizontal_points
         .iter()
-        .filter_map(|line| line.chars().nth(col))
-        .skip(row)
-        .take(row_offset + 1)
-        .all(|c| c == '+' || c == '|')
-        .then(|| ())?;
-
-    // right
-    input
-        .iter()
-        .filter_map(|line| line.chars().nth(col + col_offset))
-        .skip(row)
-        .take(row_offset + 1)
-        .all(|c| c == '+' || c == '|')
-        .then(|| ())?;
-
-    Some(())
+        .map(|&((row, col_left), (_, col_right))| {
+            input[row]
+                .chars()
+                .skip(col_left)
+                .take((col_right - col_left) + 1)
+        })
+        .all(|mut edge| edge.all(|c| c == '+' || c == '-'))
+        .then(|| {
+            vertical_points
+                .iter()
+                .map(|&((row_top, col), (row_bottom, _))| {
+                    input
+                        .iter()
+                        .filter_map(move |line| line.chars().nth(col))
+                        .skip(row_top)
+                        .take((row_bottom - row_top) + 1)
+                })
+                .all(|mut edge| edge.all(|c| c == '+' || c == '|'))
+        })
+        .contains(&true)
 }
 
 // with regular for loops and without early returns:
