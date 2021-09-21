46df8454716156ebe6343085a6dddad26b735a7c
Nicky Meuleman <nicky.dev@outlook.com>
2021-09-21 17:52:21 +0200

    exercism: health-statistics

---
+++ exercism/rust/health-statistics/src/lib.rs
@@ -0,0 +1,35 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub struct User {
+    name: String,
+    age: u32,
+    weight: f32,
+}
+
+impl User {
+    pub fn new(name: String, age: u32, weight: f32) -> Self {
+        User { name, age, weight }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn age(&self) -> u32 {
+        self.age
+    }
+
+    pub fn weight(&self) -> f32 {
+        self.weight
+    }
+
+    pub fn set_age(&mut self, new_age: u32) {
+        self.age = new_age;
+    }
+
+    pub fn set_weight(&mut self, new_weight: f32) {
+        self.weight = new_weight;
+    }
+}
