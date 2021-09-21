dd54daa0bfe86c54c4389e1c25107d59aba99e18
Nicky Meuleman <nicky.dev@outlook.com>
2021-09-21 17:55:10 +0200

    exercism: role-playing-game

---
+++ exercism/rust/role-playing-game/src/lib.rs
@@ -0,0 +1,33 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health == 0 {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if let Some(mana) = self.mana {
+            if mana >= mana_cost {
+                self.mana = Some(mana - mana_cost);
+                mana_cost * 2
+            } else {
+                0
+            }
+        } else {
+            self.health = self.health.saturating_sub(mana_cost);
+            0
+        }
+    }
+}
