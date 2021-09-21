d78c26bb0d6e438ac68c7cffbf6781282b2702f0
Nicky Meuleman <nicky.dev@outlook.com>
2021-09-21 17:53:39 +0200

    exercism: low-power-embedded-game

---
+++ exercism/rust/low-power-embedded-game/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+    iter.step_by(2)
+}
+
+pub struct Position(pub i16, pub i16);
+impl Position {
+    pub fn manhattan(&self) -> i16 {
+        self.0.abs() + self.1.abs()
+    }
+}
