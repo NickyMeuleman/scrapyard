586b0ba1a21ebf37575ee670338f708216b91db0
Nicky Meuleman <nicky.dev@outlook.com>
2021-11-03 17:02:40 +0100

    fix: add wasm_book www folder as files instead of submodule

---
+++ rust_wasm_book/wasm-game-of-life/www/index.js
@@ -0,0 +1,92 @@
+import { Universe } from "wasm-game-of-life";
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+// room for all cells and a 1px border for each
+canvas.width = (CELL_SIZE + 1) * width + 1;
+canvas.height = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(renderLoop);
+};
+
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+  
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+      ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+      ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+  
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+      ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+      ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+  
+    ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+// determine if a bit inside the Uint8Array is set.
+// we can't index into the array normally, since we use bits for cells now, not bytes, the Uint8Array uses bytes
+const isBitSet = (n, arr) => {
+  const byte = Math.floor(n / 8);
+  const mask = 1 << (n % 8);
+  return (arr[byte] & mask) === mask;
+};
+
+
+const drawCells = () => {
+    const pointer = universe.cells();
+    // access the memory through memory.buffer. That's an ArrayBuffer
+    const cells = new Uint8Array(memory.buffer, pointer, (width * height) / 8);
+
+    ctx.beginPath();
+
+    // draw each cell
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+          const idx = getIndex(row, col);
+    
+          ctx.fillStyle = isBitSet(idx, cells)
+            ? ALIVE_COLOR
+            : DEAD_COLOR;
+    
+          ctx.fillRect(
+            col * (CELL_SIZE + 1) + 1,
+            row * (CELL_SIZE + 1) + 1,
+            CELL_SIZE,
+            CELL_SIZE
+          );
+        }
+    }
+
+    ctx.stroke();
+};
+
+// start the infinite loop
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLoop);
