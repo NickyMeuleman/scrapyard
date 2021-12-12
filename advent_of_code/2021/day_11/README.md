23cb4e707442330d38a5cc6141ba50d8775d8363
Nicky Meuleman <nicky.dev@outlook.com>
2021-12-12 01:15:10 +0100

    aoc2021: day11 restructure to add benchmark

---
+++ advent_of_code/2021/day_11/README.md
@@ -0,0 +1,22 @@
+## Part 1
+
+```
+time:   [634.51 us 636.53 us 638.64 us]
+change: [-3.1666% -0.4612% +2.4043%] (p = 0.77 > 0.05)
+No change in performance detected.
+Found 9 outliers among 100 measurements (9.00%)
+1 (1.00%) low severe
+1 (1.00%) low mild
+4 (4.00%) high mild
+3 (3.00%) high severe
+```
+
+## Part 2
+```
+time:   [1.5255 ms 1.5629 ms 1.6020 ms]
+change: [-3.4142% -1.3750% +0.7625%] (p = 0.22 > 0.05)
+No change in performance detected.
+Found 15 outliers among 100 measurements (15.00%)
+2 (2.00%) high mild
+13 (13.00%) high severe
+```
--- advent_of_code/2021/day_11/bench/my_benchmark.rs
+++ advent_of_code/2021/day_11/bench/my_benchmark.rs
@@ -8,16 +8,25 @@ use criterion::{
 // use lib::Data;
 // use self::Data;
 
+// trying with a copy in lib.rs
+// this statement spits out something when I cargo bench
+// still has red squigglies though
+// unresolved import `day_11`
+// use of undeclared crate or module `day_11`rustc(E0432)
+// my_benchmark.rs(13, 5): use of undeclared crate or module `day_11`
+// use day_11::Data;
+use day_11::Data;
+
 const INPUT: &'static str = include_str!("../input.txt");
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let data: Data = INPUT.parse().unwrap();
+    let data: Data = black_box(INPUT.parse().unwrap());
     c.bench_function("part1", |b| {
-        b.iter(|| data.part_one(black_box(INPUT)))
+        b.iter(|| data.clone().part_one())
     });
 
     c.bench_function("part2", |b| {
-        b.iter(|| data.part_two(black_box(INPUT)))
+        b.iter(|| data.clone().part_two())
     });
 }
 
