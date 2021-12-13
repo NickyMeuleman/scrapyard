13921237ad1e81103cf9e2edb2d615f670648e1a
Nicky Meuleman <nicky.dev@outlook.com>
2021-12-13 22:15:20 +0100

    aoc2021: day13 add benchmark results

--- advent_of_code/2021/day_13/README.md
+++ advent_of_code/2021/day_13/README.md
@@ -1,3 +1,24 @@
 ## Part 1
 
-## Part 2
+### Criterion benchmark
+
+time:   [22.360 us 22.427 us 22.503 us]
+change: [-11.248% -8.3959% -5.4062%] (p = 0.00 < 0.05)
+Performance has improved.
+Found 11 outliers among 100 measurements (11.00%)
+1 (1.00%) low mild
+6 (6.00%) high mild
+4 (4.00%) high severe
+
+## Part
+
+### Criterion benchmark
+
+part2
+time:   [129.24 us 129.60 us 129.97 us]
+change: [-85.320% -81.431% -75.953%] (p = 0.00 < 0.05)
+Performance has improved.
+Found 7 outliers among 100 measurements (7.00%)
+1 (1.00%) low mild
+2 (2.00%) high mild
+4 (4.00%) high severe
--- advent_of_code/2021/day_13/src/main.rs
+++ advent_of_code/2021/day_13/src/main.rs
@@ -2,22 +2,7 @@ use day_13::Data;
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("./input_snoozeysleepy.txt").unwrap();
-    // let input = fs::read_to_string("./input_mohammedsouleymane.txt").unwrap();
-    // let input = fs::read_to_string("./input_chrisbiscardi.txt").unwrap();
-    // let input = fs::read_to_string("./input_philds.txt").unwrap();
-    // let input = fs::read_to_string("./input_jesperdramsch.txt").unwrap();
-    // let input = fs::read_to_string("./input_marcelblijleven.txt").unwrap();
-    // let input = fs::read_to_string("./input_tbpaolini.txt").unwrap();
-    // let input = fs::read_to_string("./input_snhmibby.txt").unwrap();
-    // let input = fs::read_to_string("./input_tginsberg.txt").unwrap();
-    // let input = fs::read_to_string("./input_letfish.txt").unwrap();
-    // let input = fs::read_to_string("./input_linagkar.txt").unwrap();
-    // let input = fs::read_to_string("./input_jeffomatic.txt").unwrap();
-    // let input = fs::read_to_string("./input_misfits42.txt").unwrap();
-    // let input = fs::read_to_string("./input_fadi88.txt").unwrap();
-    // let input = fs::read_to_string("./input_gobler.txt").unwrap();
-    // let input = fs::read_to_string("./input.txt").unwrap();
+    let input = fs::read_to_string("./input.txt").unwrap();
     let data: Data = input.parse().unwrap();
     println!("Part one answer: {}", data.clone().part_one());
     println!("Part two answer: {}", data.part_two());
