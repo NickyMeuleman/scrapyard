61986634947ca5de45cd077835a75c144583c72a
Nicky Meuleman <nicky.dev@outlook.com>
2021-12-26 16:01:43 +0100

    aoc2021: day01 edits

---
+++ advent_of_code/2021/day_01/README.md
@@ -0,0 +1,24 @@
+## Part 1
+
+### Criterion benchmark
+
+```
+part1
+time:   [597.22 ns 600.29 ns 603.79 ns]
+Found 13 outliers among 100 measurements (13.00%)
+3 (3.00%) low mild
+2 (2.00%) high mild
+8 (8.00%) high severe
+```
+
+## Part 2
+
+### Criterion benchmark
+
+```
+part2
+time:   [5.4335 us 5.4534 us 5.4744 us]
+Found 6 outliers among 100 measurements (6.00%)
+4 (4.00%) high mild
+2 (2.00%) high severe
+```
--- advent_of_code/2021/day_01/src/main.rs
+++ advent_of_code/2021/day_01/src/main.rs
@@ -1,30 +1,9 @@
-use std::cmp::Ordering;
+use day_01::Data;
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
-    let data = parse(&input);
-    println!("Part one answer: {}", part_one(data.clone()));
-    println!("Part two answer: {}", part_two(data));
-}
-
-fn parse(input: &str) -> Vec<u32> {
-    input.lines().filter_map(|s| s.parse().ok()).collect()
-}
-
-fn part_one(data: Vec<u32>) -> usize {
-    data.windows(2)
-        .filter(|window| window[0].cmp(&window[1]) == Ordering::Less)
-        .count()
-}
-
-fn part_two(data: Vec<u32>) -> usize {
-    // turn data into sums of three-measurement windows
-    data.windows(3)
-        .map(|window| window.iter().sum())
-        .collect::<Vec<u32>>()
-        // count the amount of times a three-measurement sum increases
-        .windows(2)
-        .filter(|window| window[0].cmp(&window[1]) == Ordering::Less)
-        .count()
+    let data: Data = input.parse().unwrap();
+    println!("Part one answer: {}", data.part_one());
+    println!("Part two answer: {}", data.part_two());
 }
