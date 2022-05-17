6f21440734a54e9b606e77e08eae42ee07a92829
Nicky Meuleman <nicky.dev@outlook.com>
2022-05-17 16:33:42 +0200

    aoc2021: replace with redone files

--- advent_of_code/2021/day_01/src/main.rs
+++
@@ -1,9 +0,0 @@
-use day_01::Data;
-use std::fs;
-
-fn main() {
-    let input = fs::read_to_string("./input.txt").unwrap();
-    let data: Data = input.parse().unwrap();
-    println!("Part one answer: {}", data.part_one());
-    println!("Part two answer: {}", data.part_two());
-}
--- advent_of_code/2021/day_11/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-use day_11::Data;
-
-fn main() {
-    let input = fs::read_to_string("./input.txt").unwrap();
-    let data: Data = input.parse().unwrap();
-    println!("Part one answer: {}", data.clone().part_one());
-    println!("Part two answer: {}", data.part_two());
-}
