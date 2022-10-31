668a520a130b9fbf0f30f1aad25e270b2fa3594d
Nicky Meuleman <nicky.dev@outlook.com>
2022-10-31 21:16:47 +0100

    aoc2015: day17

--- advent_of_code/2015/src/utils.rs
+++ advent_of_code/2015/src/utils.rs
@@ -1,6 +1,6 @@
 use crate::{
     day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10, day_11, day_12,
-    day_13, day_14, day_15,
+    day_13, day_14, day_15, day_16, day_17,
 };
 
 use std::fs;
@@ -105,8 +105,8 @@ pub fn run(day: u8, input: String) -> Result<Solution, JsError> {
         13 => run_day::<day_13::Data>(input),
         14 => run_day::<day_14::Data>(input),
         15 => run_day::<day_15::Data>(input),
-        // 16 => run_day::<day_16::Data>(input),
-        // 17 => run_day::<day_17::Data>(input),
+        16 => run_day::<day_16::Data>(input),
+        17 => run_day::<day_17::Data>(input),
         // 18 => run_day::<day_18::Data>(input),
         // 19 => run_day::<day_19::Data>(input),
         // 20 => run_day::<day_20::Data>(input),
