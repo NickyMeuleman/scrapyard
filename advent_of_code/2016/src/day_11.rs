1940bce616c91be10ba5a4b6e7e93ddc16b07eca
Nicky Meuleman <nicky.dev@outlook.com>
2022-11-15 16:09:10 +0100

    aoc2016: day11 debugging

--- advent_of_code/2016/src/day_11.rs
+++ advent_of_code/2016/src/day_11.rs
@@ -1,7 +1,10 @@
 use std::{
     cmp::Ordering,
     collections::{BinaryHeap, HashSet},
+    fmt,
 };
+
+use itertools::Itertools;
 
 use crate::AoCData;
 
@@ -9,10 +12,43 @@ pub struct Data {
     floors: Vec<HashSet<Item>>,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone)]
 struct State {
-    elevator: u8,
-    floors: Vec<HashSet<Item>>,
+    elevator: i8,
+    floors: [Floor; 4],
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Floor {
+    items: HashSet<Item>,
+}
+
+impl Floor {
+    fn is_valid(&self) -> bool {
+        let (gens, chips): (Vec<_>, Vec<_>) = self.items.iter().partition(|item| match item {
+            Item::Generator(_) => true,
+            Item::Microchip(_) => false,
+        });
+        let has_single_item_type = gens.is_empty() || chips.is_empty();
+        let all_chips_paired = self
+            .items
+            .iter()
+            .filter_map(|item| match item {
+                Item::Microchip(name) => Some(name),
+                Item::Generator(_) => None,
+            })
+            .all(|chip_name| self.items.contains(&Item::Generator(chip_name.to_string())));
+
+        self.items.is_empty() || has_single_item_type || all_chips_paired
+    }
+
+    fn counts(&self) -> (u8, u8) {
+        let (gens, chips): (Vec<_>, Vec<_>) = self.items.iter().partition(|item| match item {
+            Item::Generator(_) => true,
+            Item::Microchip(_) => false,
+        });
+        (gens.len() as u8, chips.len() as u8)
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -33,43 +69,156 @@ impl PartialOrd for Node {
     }
 }
 
+// test input needs a , before the last and on the first line
+// only for the test input because I can't see where it's going wrong
+impl fmt::Debug for State {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut result = String::new();
+        result.push('\n');
+        for floor in (0..=3).rev() {
+            let mut line = String::new();
+            line.push_str(&format!("F{}  ", floor + 1));
+            let e = if self.elevator == floor as i8 {
+                " E "
+            } else {
+                " . "
+            };
+            line.push_str(e);
+            let floor = &self.floors[floor];
+            let hg = if floor
+                .items
+                .contains(&Item::Generator("hydrogen".to_string()))
+            {
+                " HG "
+            } else {
+                " .  "
+            };
+            let hm = if floor
+                .items
+                .contains(&Item::Microchip("hydrogen".to_string()))
+            {
+                " HM "
+            } else {
+                " .  "
+            };
+            let lg = if floor
+                .items
+                .contains(&Item::Generator("lithium".to_string()))
+            {
+                " LG "
+            } else {
+                " .  "
+            };
+            let lm = if floor
+                .items
+                .contains(&Item::Microchip("lithium".to_string()))
+            {
+                " LM "
+            } else {
+                " .  "
+            };
+            line.push_str(hg);
+            line.push_str(hm);
+            line.push_str(lg);
+            line.push_str(lm);
+            line.push('\n');
+            result.push_str(&line);
+        }
+        write!(f, "{}", result)
+    }
+}
+
 impl State {
     /// creates state where elevator starts at the bottom
-    fn new(floors: Vec<HashSet<Item>>) -> Self {
-        Self {
+    fn try_new(floors: Vec<HashSet<Item>>) -> Option<Self> {
+        Some(Self {
             elevator: 0,
-            floors,
-        }
+            floors: floors
+                .into_iter()
+                .map(|items| Floor { items })
+                .collect::<Vec<Floor>>()
+                .try_into()
+                .ok()?,
+        })
     }
     /// checks if top floor is full by checking the first 3 floors are empty
     fn full_last_level(&self) -> bool {
-        self.floors.iter().take(3).all(|set| set.is_empty())
-    }
-
-    /// check if floor at idx is valid
-    fn is_valid_floor(&self, idx: usize) -> bool {
-        let floor = &self.floors[idx];
-        let all_chips_safe = floor
+        self.floors
             .iter()
-            .filter_map(|item| match item {
-                Item::Microchip(name) => Some(name),
-                Item::Generator(_) => None,
-            })
-            .all(|chip_name| floor.contains(&Item::Generator(chip_name.to_string())));
-
-        floor.is_empty() || floor.len() == 1 || all_chips_safe
+            .take(self.floors.len() - 1)
+            .all(|floor| floor.items.is_empty())
     }
 
     /// returns all legal next states from a given state
     fn next_states(&self) -> Vec<State> {
-        unimplemented!()
+        let highest_uncleared_floor = self
+            .floors
+            .iter()
+            .position(|floor| !floor.items.is_empty())
+            .unwrap();
+        let mut possibilities: Vec<State> = Vec::new();
+        let floor = &self.floors[self.elevator as usize];
+
+        let moves = floor
+            .items
+            .iter()
+            // all combinations where you take 2 items
+            .combinations(2)
+            // all combinations where you take 1 item
+            .chain(floor.items.iter().combinations(1));
+
+        for items in moves {
+            // move them up or down
+            for direction in [-1, 1] {
+                let next_elevator = self.elevator + direction;
+                // make sure elevator stays within bounds
+                if !(0..=3).contains(&next_elevator) {
+                    continue;
+                }
+                // don't ever bring things down to a cleared level
+                if (next_elevator as usize) < highest_uncleared_floor {
+                    continue;
+                }
+                let mut next_floors = self.floors.clone();
+                for &item in items.iter() {
+                    // remove item from old floor
+                    next_floors[self.elevator as usize].items.remove(item);
+                    // add item to new floor
+                    next_floors[next_elevator as usize]
+                        .items
+                        .insert(item.clone());
+                }
+                // only add new state if both the old floor and the new floor are valid
+                if next_floors[self.elevator as usize].is_valid()
+                    && next_floors[next_elevator as usize].is_valid()
+                {
+                    possibilities.push(State {
+                        elevator: next_elevator,
+                        floors: next_floors,
+                    });
+                }
+            }
+        }
+
+        possibilities
+    }
+
+    fn counts(&self) -> [(u8, u8); 4] {
+        self.floors
+            .iter()
+            .map(|floor| floor.counts())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap()
     }
 
     /// minimum cost to move every item to the top
     fn min_cost_to_top(self) -> u32 {
-        // init min_cost to end and priority queue
         let mut min_cost = u32::MAX;
+        // Each VALID floor can be represented by (num_gens, num_chips) as each pair is interchangeable
+        let mut seen = HashSet::new();
         let mut pq = BinaryHeap::new();
+
         // add starting node to pq
         pq.push(Node {
             cost: 0,
@@ -77,17 +226,27 @@ impl State {
         });
 
         'outer: while let Some(Node { cost, state }) = pq.pop() {
+            dbg!(&cost);
+            dbg!(&state);
+            dbg!(&pq.len());
+            dbg!("");
+
             if state.full_last_level() {
                 // popped an endstate, return
                 return cost;
             }
-            if cost > min_cost {
+
+            seen.insert(state.counts());
+
+            if cost >= min_cost {
                 // a less costly solution has already been found, skip
-                continue;
+                // this happens when there's a solution with a lower cost in the queue
+                continue 'outer;
             }
 
             for new_state in state.next_states() {
                 let new_cost = cost + 1;
+
                 if new_state.full_last_level() {
                     // do not return here, the route to the end might not be the cheapest one
                     min_cost = min_cost.min(new_cost);
@@ -99,10 +258,12 @@ impl State {
                 }
 
                 // if we reached this, the items are not at the top yet, keep going with this branch
-                pq.push(Node {
-                    cost: new_cost,
-                    state: new_state,
-                });
+                if !seen.contains(&new_state.counts()) {
+                    pq.push(Node {
+                        cost: new_cost,
+                        state: new_state,
+                    });
+                }
             }
         }
 
@@ -164,7 +325,8 @@ impl AoCData for Data {
     }
 
     fn part_1(&self) -> String {
-        State::new(self.floors.clone())
+        State::try_new(self.floors.clone())
+            .unwrap()
             .min_cost_to_top()
             .to_string()
     }
