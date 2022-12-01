ab889eabec30a1e31d26c171d4853d786404ae37
Nicky Meuleman <nicky.dev@outlook.com>
2022-12-01 01:49:46 +0100

    aoc2016: day20 use input from file

--- advent_of_code/2015/src/day_20.rs
+++ advent_of_code/2015/src/day_20.rs
@@ -1,14 +1,14 @@
 use crate::AoCData;
 
-pub struct Data {}
-const TARGET: usize = 36_000_000;
+pub struct Data(usize);
+
 impl AoCData for Data {
     fn try_new(input: String) -> Option<Self> {
-        Some(Self {})
+        Some(Self(input.parse().ok()?))
     }
 
     fn part_1(&self) -> String {
-        let mut houses = vec![0; TARGET / 10 + 1];
+        let mut houses = vec![0; &self.0 / 10 + 1];
         for elf in 1..houses.len() {
             for house in (elf..houses.len()).step_by(elf) {
                 houses[house] += elf * 10;
@@ -17,13 +17,13 @@ impl AoCData for Data {
 
         houses
             .iter()
-            .position(|x| x >= &TARGET)
+            .position(|x| x >= &self.0)
             .unwrap()
             .to_string()
     }
 
     fn part_2(&self) -> String {
-        let mut houses = vec![0; TARGET / 10 + 1];
+        let mut houses = vec![0; &self.0 / 10 + 1];
         for elf in 1..houses.len() {
             let max_house = houses.len().min(elf * 50);
             for house in (elf..max_house).step_by(elf) {
@@ -33,7 +33,7 @@ impl AoCData for Data {
 
         houses
             .iter()
-            .position(|x| x >= &TARGET)
+            .position(|x| x >= &self.0)
             .unwrap()
             .to_string()
     }
@@ -46,15 +46,15 @@ mod test {
 
     #[test]
     fn part_1() {
-        let input = utils::get_sample_input(1);
+        let input = utils::get_input(20);
         let data = Data::try_new(input).unwrap();
-        assert_eq!(data.part_1(), "");
+        assert_eq!(data.part_1(), "831600");
     }
 
     #[test]
     fn part_2() {
-        let input = utils::get_sample_input(1);
+        let input = utils::get_input(20);
         let data = Data::try_new(input).unwrap();
-        assert_eq!(data.part_2(), "");
+        assert_eq!(data.part_2(), "884520");
     }
 }
