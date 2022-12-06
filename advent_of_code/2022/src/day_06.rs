ac66810e354e1f91b0e1fee672f63f4165b6d21b
Nicky Meuleman <nicky.dev@outlook.com>
2022-12-06 14:52:04 +0100

    aoc2022: day06

--- advent_of_code/2022/src/day_06.rs
+++ advent_of_code/2022/src/day_06.rs
@@ -1,18 +1,31 @@
 use crate::AoCData;
 
-pub struct Data {}
+pub struct Data(String);
+
+fn helper(s: &str, size: usize) -> usize {
+    s.as_bytes()
+        .windows(size)
+        .position(|window| {
+            window
+                .iter()
+                .enumerate()
+                .all(|(idx, c)| !window[..idx].contains(c))
+        })
+        .unwrap_or(s.len())
+        + size
+}
 
 impl AoCData for Data {
     fn try_new(input: String) -> Option<Self> {
-        Some(Self {})
+        Some(Self(input))
     }
 
     fn part_1(&self) -> String {
-        String::new()
+        helper(&self.0, 4).to_string()
     }
 
     fn part_2(&self) -> String {
-        String::new()
+        helper(&self.0, 14).to_string()
     }
 }
 
@@ -25,13 +38,13 @@ mod test {
     fn part_1() {
         let input = utils::get_sample_input(6);
         let data = Data::try_new(input).unwrap();
-        assert_eq!(data.part_1(), "");
+        assert_eq!(data.part_1(), "11");
     }
 
     #[test]
     fn part_2() {
         let input = utils::get_sample_input(6);
         let data = Data::try_new(input).unwrap();
-        assert_eq!(data.part_2(), "");
+        assert_eq!(data.part_2(), "26");
     }
 }
