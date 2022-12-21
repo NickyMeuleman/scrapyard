3f73beb92d1d575fecb90016f713652172da0ae1
Nicky Meuleman <nicky.dev@outlook.com>
2022-12-21 02:50:45 +0100

    aoc2022: day19 recursive version

--- advent_of_code/2022/src/day_19.rs
+++ advent_of_code/2022/src/day_19.rs
@@ -13,6 +13,182 @@ struct State {
     elapsed: u16,
 }
 
+// START QUEUE VERSION
+// fn max_geodes(blueprint: &[[u16; 4]; 4], max_time: u16) -> u16 {
+//     // calculate the maximum amount for every type of bot so that the creation of a new bot of any type is never bottlenecked
+//     // it doesn't make sense to build more bots than that maximum if the resources a bot type generates are
+//     // enough to cover that type (ore, clay, obsidian) cost for any possible bot (per question, you can only build 1 bot per turn)
+//     // for geode bots, there is no logical maximum amount
+//     // [ore, clay, obsidian, geode]
+//     let mut max_robots = [u16::MAX; 4];
+//     for i in 0..3 {
+//         max_robots[i] = blueprint.iter().map(|cost| cost[i]).max().unwrap();
+//     }
+//     let mut max_geodes = 0;
+
+//     let mut q = VecDeque::new();
+//     q.push_back(State {
+//         inventory: [0, 0, 0, 0],
+//         bots: [1, 0, 0, 0],
+//         elapsed: 0,
+//     });
+
+//     while let Some(State {
+//         inventory,
+//         bots,
+//         elapsed,
+//     }) = q.pop_front()
+//     {
+//         // for every bot cost, run simulation
+//         for i in 0..blueprint.len() {
+//             // if we already have enough of this bot type, skip
+//             if bots[i] == max_robots[i] {
+//                 continue;
+//             }
+
+//             let costs = &blueprint[i];
+
+//             // Find the limiting resource type for the costs.
+//             let wait_time = (0..3)
+//                 .map(|idx| {
+//                     match costs[idx] {
+//                         // state has enough of current resource in inventory to cover that part of the target bot cost. 0 wait time
+//                         cost if cost <= inventory[idx] => 0,
+//                         // no target bot type made yet
+//                         // we can't build it (it takes more than max_time to build it).
+//                         _ if bots[idx] == 0 => max_time + 1,
+//                         _ => (costs[idx] - inventory[idx] + bots[idx] - 1) / bots[idx],
+//                     }
+//                 })
+//                 .max()
+//                 .unwrap();
+
+//             // if that choice would cause the time limit be to exceeded, skip
+//             // the + 1 is so the built bot has the chance to do something, it merely being built is not enough
+//             let new_elapsed = elapsed + wait_time + 1;
+//             if new_elapsed >= max_time {
+//                 continue;
+//             }
+
+//             // gather ores with previously available bots
+//             let mut new_inventory = [0; 4];
+//             for idx in 0..bots.len() {
+//                 new_inventory[idx] = inventory[idx] + bots[idx] * (wait_time + 1) - costs[idx];
+//             }
+
+//             // increase bot type for the bot we just built
+//             let mut new_bots = bots;
+//             new_bots[i] += 1;
+
+//             // extra optimization:
+//             // if we theoretically only built geode bots every turn, and we still don't beat the maximum, skip
+//             let remaining_time = max_time - new_elapsed;
+//             if ((remaining_time - 1) * remaining_time) / 2
+//                 + new_inventory[3]
+//                 + remaining_time * new_bots[3]
+//                 < max_geodes
+//             {
+//                 continue;
+//             }
+
+//             q.push_back(State {
+//                 inventory: new_inventory,
+//                 bots: new_bots,
+//                 elapsed: new_elapsed,
+//             })
+//         }
+
+//         let geodes = inventory[3] + bots[3] * (max_time - elapsed);
+//         max_geodes = geodes.max(max_geodes);
+//     }
+
+//     max_geodes
+// }
+
+// END QUEUE VERSION
+// START RECURSIVE VERSION
+
+// you can also completely remove the `has_recursed` boolean and always do the logic instead of conditionally
+// that also works and would make the recursive version identical in logic to the queue based version
+
+fn recurse(
+    blueprint: &[[u16; 4]; 4],
+    max_time: u16,
+    max_robots: &[u16; 4],
+    state: State,
+    max_geodes: &mut u16,
+) {
+    let State {
+        inventory,
+        bots,
+        elapsed,
+    } = state;
+    let mut recursed = false;
+    // for every bot cost, run simulation
+    for i in 0..blueprint.len() {
+        // if we already have enough of this bot type, skip
+        if bots[i] == max_robots[i] {
+            continue;
+        }
+
+        let costs = &blueprint[i];
+
+        // Find the limiting resource type for the costs.
+        let wait_time = (0..3)
+            .map(|idx| {
+                match costs[idx] {
+                    // state has enough of current resource in inventory to cover that part of the target bot cost. 0 wait time
+                    cost if cost <= inventory[idx] => 0,
+                    // no target bot type made yet
+                    // we can't build it (it takes more than max_time to build it).
+                    _ if bots[idx] == 0 => max_time + 1,
+                    _ => (costs[idx] - inventory[idx] + bots[idx] - 1) / bots[idx],
+                }
+            })
+            .max()
+            .unwrap();
+
+        // if that choice would cause the time limit be to exceeded, skip
+        // the + 1 is so the built bot has the chance to do something, it merely being built is not enough
+        let new_elapsed = elapsed + wait_time + 1;
+        if new_elapsed >= max_time {
+            continue;
+        }
+
+        // gather ores with previously available bots
+        let mut new_inventory = [0; 4];
+        for idx in 0..bots.len() {
+            new_inventory[idx] = inventory[idx] + bots[idx] * (wait_time + 1) - costs[idx];
+        }
+
+        // increase bot type for the bot we just built
+        let mut new_bots = bots;
+        new_bots[i] += 1;
+
+        // extra optimization:
+        // if we theoretically only built geode bots every turn, and we still don't beat the maximum, skip
+        let remaining_time = max_time - new_elapsed;
+        if ((remaining_time - 1) * remaining_time) / 2
+            + new_inventory[3]
+            + remaining_time * new_bots[3]
+            < *max_geodes
+        {
+            continue;
+        }
+
+        let new_state = State {
+            inventory: new_inventory,
+            bots: new_bots,
+            elapsed: new_elapsed,
+        };
+        recursed = true;
+        recurse(blueprint, max_time, max_robots, new_state, max_geodes)
+    }
+    if !recursed {
+        let geodes = inventory[3] + bots[3] * (max_time - elapsed);
+        *max_geodes = geodes.max(*max_geodes);
+    }
+}
 fn max_geodes(blueprint: &[[u16; 4]; 4], max_time: u16) -> u16 {
     // calculate the maximum amount for every type of bot so that the creation of a new bot of any type is never bottlenecked
     // it doesn't make sense to build more bots than that maximum if the resources a bot type generates are
@@ -25,84 +201,18 @@ fn max_geodes(blueprint: &[[u16; 4]; 4], max_time: u16) -> u16 {
     }
     let mut max_geodes = 0;
 
-    let mut q = VecDeque::new();
-    q.push_back(State {
+    let state = State {
         inventory: [0, 0, 0, 0],
         bots: [1, 0, 0, 0],
         elapsed: 0,
-    });
+    };
 
-    while let Some(State {
-        inventory,
-        bots,
-        elapsed,
-    }) = q.pop_front()
-    {
-        // for every bot cost, run simulation
-        for i in 0..blueprint.len() {
-            // if we already have enough of this bot type, skip
-            if bots[i] == max_robots[i] {
-                continue;
-            }
-
-            let costs = &blueprint[i];
-
-            // Find the limiting resource type for the costs.
-            let wait_time = (0..3)
-                .map(|idx| {
-                    match costs[idx] {
-                        // state has enough of current resource in inventory to cover that part of the target bot cost. 0 wait time
-                        cost if cost <= inventory[idx] => 0,
-                        // no target bot type made yet
-                        // we can't build it (it takes more than max_time to build it).
-                        _ if bots[idx] == 0 => max_time + 1,
-                        _ => (costs[idx] - inventory[idx] + bots[idx] - 1) / bots[idx],
-                    }
-                })
-                .max()
-                .unwrap();
-
-            // if that choice would cause the time limit be to exceeded, skip
-            // the + 1 is so the built bot has the chance to do something, it merely being built is not enough
-            let new_elapsed = elapsed + wait_time + 1;
-            if new_elapsed >= max_time {
-                continue;
-            }
-
-            // gather ores with previously available bots
-            let mut new_inventory = [0; 4];
-            for idx in 0..bots.len() {
-                new_inventory[idx] = inventory[idx] + bots[idx] * (wait_time + 1) - costs[idx];
-            }
-
-            // increase bot type for the bot we just built
-            let mut new_bots = bots.clone();
-            new_bots[i] += 1;
-
-            // extra optimization:
-            // if we theoretically only built geode bots every turn, and we still don't beat the maximum, skip
-            let remaining_time = max_time - new_elapsed;
-            if ((remaining_time - 1) * remaining_time) / 2
-                + new_inventory[3]
-                + remaining_time * new_bots[3]
-                < max_geodes
-            {
-                continue;
-            }
-
-            q.push_back(State {
-                inventory: new_inventory,
-                bots: new_bots,
-                elapsed: new_elapsed,
-            })
-        }
-
-        let geodes = inventory[3] + bots[3] * (max_time - elapsed);
-        max_geodes = geodes.max(max_geodes);
-    }
+    recurse(blueprint, max_time, &max_robots, state, &mut max_geodes);
 
     max_geodes
 }
+
+// END RECURSIVE VERSION
 
 impl AoCData for Data {
     fn try_new(input: String) -> Option<Self> {
