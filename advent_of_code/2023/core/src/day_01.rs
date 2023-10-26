80153a4e48f7def2c5f56316120ae973a8599c9a
Nicky Meuleman <nicky.dev@outlook.com>
2023-10-26 21:48:30 +0200

    clear day1

--- advent_of_code/2023/core/src/day_01.rs
+++ advent_of_code/2023/core/src/day_01.rs
@@ -3,46 +3,19 @@ use std::fmt::Display;
 use crate::AoCDay;
 
 #[derive(Debug, Clone)]
-pub struct Data(Vec<u32>);
+pub struct Data;
 
 impl AoCDay<'_> for Data {
-    fn try_new(input: &str) -> Option<Self> {
-        Some(Self(
-            input
-                .lines()
-                .filter_map(|line| line.parse().ok())
-                .collect(),
-        ))
+    fn try_new(_input: &str) -> Option<Self> {
+        Some(Self)
     }
 
     fn part_1(&self) -> impl Display {
-        self.0
-            .iter()
-            .map(|mass| mass / 3 - 2)
-            .sum::<u32>()
+        ""
     }
 
     fn part_2(&self) -> impl Display {
-        fn needed_fuel(mass: u32) -> u32 {
-            (mass / 3).saturating_sub(2)
-        }
-
-        // iteration
-        let mut total = 0;
-        for mass in &self.0 {
-            let mut previous = needed_fuel(*mass);
-            total += previous;
-            loop {
-                let needed = needed_fuel(previous);
-                if needed > 0 {
-                    total += needed;
-                    previous = needed;
-                } else {
-                    break;
-                }
-            }
-        }
-        total
+        ""
     }
 }
 
