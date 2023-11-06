edcfd529b524dd8e2f552ccd5468fc58726f31cd
Nicky Meuleman <nicky.dev@outlook.com>
2023-11-06 21:43:10 +0100

    aoc2017 day16

--- advent_of_code/2017/solutions/src/day_16.rs
+++ advent_of_code/2017/solutions/src/day_16.rs
@@ -1,41 +1,121 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
+
+use aoc_core::AoCError;
 
 use crate::{AoCData, AoCResult};
 
 #[derive(Debug, Clone)]
-pub struct Data;
+pub struct Data(Vec<Instruction>);
+
+#[derive(Debug, Clone)]
+enum Instruction {
+    Spin(usize),
+    Exchange(usize, usize),
+    Parner(char, char),
+}
 
 impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+    fn try_new(input: &str) -> AoCResult<Self> {
+        let result = input
+            .trim()
+            .split(",")
+            .map(|s| {
+                let (kind, rest) = s.split_at(1);
+                match kind {
+                    "s" => {
+                        let num = rest.parse()?;
+                        Ok(Instruction::Spin(num))
+                    }
+                    "x" => {
+                        let (left, right) = rest
+                            .split_once("/")
+                            .ok_or(AoCError::Parsing)?;
+                        let left = left.parse()?;
+                        let right = right.parse()?;
+                        Ok(Instruction::Exchange(left, right))
+                    }
+                    "p" => {
+                        let (left, right) = rest
+                            .split_once("/")
+                            .ok_or(AoCError::Parsing)?;
+                        Ok(Instruction::Parner(
+                            left.chars()
+                                .next()
+                                .ok_or(AoCError::Parsing)?,
+                            right
+                                .chars()
+                                .next()
+                                .ok_or(AoCError::Parsing)?,
+                        ))
+                    }
+                    _ => return Err(AoCError::Parsing),
+                }
+            })
+            .collect::<AoCResult<_>>()?;
+
+        Ok(Self(result))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let mut programs: Vec<char> = ('a'..='p').collect();
+        let len = programs.len();
+        for inst in self.0.iter() {
+            match inst {
+                Instruction::Spin(n) => programs.rotate_left(len - n),
+                Instruction::Exchange(a, b) => programs.swap(*a, *b),
+                Instruction::Parner(a, b) => {
+                    let a = programs
+                        .iter()
+                        .position(|c| c == a)
+                        .ok_or(AoCError::Solving)?;
+                    let b = programs
+                        .iter()
+                        .position(|c| c == b)
+                        .ok_or(AoCError::Solving)?;
+                    programs.swap(a, b);
+                }
+            }
+        }
+
+        Ok(programs.iter().collect::<String>())
     }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
-    }
-}
+        let target = 1_000_000_000;
+        let mut count = 0;
+        let mut programs: Vec<char> = ('a'..='p').collect();
+        let len = programs.len();
+        let mut seen = HashMap::new();
+        while count < target {
+            for inst in self.0.iter() {
+                match inst {
+                    Instruction::Spin(n) => programs.rotate_left(len - n),
+                    Instruction::Exchange(a, b) => programs.swap(*a, *b),
+                    Instruction::Parner(a, b) => {
+                        let a = programs
+                            .iter()
+                            .position(|c| c == a)
+                            .ok_or(AoCError::Solving)?;
+                        let b = programs
+                            .iter()
+                            .position(|c| c == b)
+                            .ok_or(AoCError::Solving)?;
+                        programs.swap(a, b);
+                    }
+                }
+            }
+            // look for cycle
+            let key: String = programs.iter().collect();
+            if let Some(prev_count) = seen.get(&key) {
+                let delta = count - prev_count;
+                let repeats = (target - count) / delta;
+                count += repeats * delta;
+            }
+            // update seen map
+            seen.entry(key).or_insert(count);
+            count += 1;
+        }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn part_1() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
-    }
-
-    #[test]
-    fn part_2() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        Ok(programs.iter().collect::<String>())
     }
 }
