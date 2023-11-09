13a0252ec7bbebc501e375ac55a704f334227121
Nicky Meuleman <nicky.dev@outlook.com>
2023-11-09 21:15:48 +0100

    aoc2018 day02

--- advent_of_code/2018/solutions/src/day_02.rs
+++ advent_of_code/2018/solutions/src/day_02.rs
@@ -1,41 +1,84 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
+
+use itertools::Itertools;
 
 use crate::{AoCData, AoCResult};
 
 #[derive(Debug, Clone)]
-pub struct Data;
+pub struct Data<'a>(&'a str);
 
-impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+impl<'a> AoCData<'a> for Data<'a> {
+    fn try_new(input: &'a str) -> AoCResult<Self> {
+        Ok(Self(input))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let counts: Vec<HashMap<char, u32>> = self
+            .0
+            .lines()
+            .map(|line| {
+                let hash_map = HashMap::new();
+                line.chars()
+                    .fold(hash_map, |mut acc, c| {
+                        *acc.entry(c).or_default() += 1;
+                        acc
+                    })
+            })
+            .collect();
+        let has_double = counts
+            .iter()
+            .filter(|map| map.values().contains(&2))
+            .count();
+        let has_triple = counts
+            .iter()
+            .filter(|map| map.values().contains(&3))
+            .count();
+
+        Ok(has_double * has_triple)
     }
+
+    // cooler but slower (because of extra allocation?)
+    // pub fn part_2(input: &str) -> String {
+    //     input
+    //         .lines()
+    //         .tuple_combinations()
+    //         .find_map(|(first, second)| {
+    //             let (same, different): (Vec<_>, Vec<_>) = first
+    //                 .chars()
+    //                 .zip(second.chars())
+    //                 .partition(|(c1, c2)| c1 == c2);
+    //             if different.len() == 1 {
+    //                 Some(same)
+    //             } else {
+    //                 None
+    //             }
+    //         })
+    //         .map(|same| same.into_iter().map(|(c1, _)| c1).collect())
+    //         .unwrap()
+    // }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
-    }
-}
+        let (first, second) = self
+            .0
+            .lines()
+            .tuple_combinations()
+            .find(|(first, second)| {
+                let dist = first
+                    .chars()
+                    .zip(second.chars())
+                    .filter(|(c1, c2)| c1 != c2)
+                    .count();
+                dist == 1
+            })
+            .unwrap();
 
-#[cfg(test)]
-mod test {
-    use super::*;
+        let result: String = first
+            .chars()
+            .zip(second.chars())
+            .filter(|(c1, c2)| c1 == c2)
+            .map(|(c1, _)| c1)
+            .collect();
 
-    #[test]
-    fn part_1() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
-    }
-
-    #[test]
-    fn part_2() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        Ok(result)
     }
 }
