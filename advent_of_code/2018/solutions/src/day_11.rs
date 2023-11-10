ca73c9d1519ff3b3d54577788f206b5cf06da8fd
Nicky Meuleman <nicky.dev@outlook.com>
2023-11-10 16:35:01 +0100

    aoc2018 day11 and 12

--- advent_of_code/2018/solutions/src/day_11.rs
+++ advent_of_code/2018/solutions/src/day_11.rs
@@ -1,21 +1,168 @@
 use std::fmt::Display;
 
+use aoc_core::{AoCError, Solution};
+use itertools::Itertools;
+
 use crate::{AoCData, AoCResult};
 
-#[derive(Debug, Clone)]
-pub struct Data;
+const SIZE: usize = 300;
 
+#[derive(Debug, Clone)]
+pub struct Data(i32);
+
+// Takes very long to execute
 impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+    fn try_new(input: &str) -> AoCResult<Self> {
+        Ok(Self(input.trim().parse()?))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let map: Vec<Vec<_>> = (1..=SIZE as i32)
+            .map(|x| {
+                (1..=SIZE as i32)
+                    .map(move |y| {
+                        // Find the fuel cell's rack ID,
+                        // which is its X coordinate plus 10.
+                        let rack_id = x + 10;
+                        // Begin with a power level of the rack ID times the Y coordinate.
+                        let mut power_level = rack_id * y;
+                        // Increase the power level by the value of the grid serial number (your puzzle input).
+                        power_level += self.0;
+                        // Set the power level to itself multiplied by the rack ID.
+                        power_level *= rack_id;
+                        // Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+                        power_level /= 100;
+                        power_level %= 10;
+                        // Subtract 5 from the power level.
+                        power_level - 5
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let (x, y) = (1..=SIZE - 2)
+            .cartesian_product(1..=SIZE - 2)
+            .max_by_key(|(x, y)| {
+                (0..3)
+                    .map(|dx| {
+                        (0..3)
+                            .map(|dy| map[x - 1 + dx][y - 1 + dy])
+                            .sum::<i32>()
+                    })
+                    .sum::<i32>()
+            })
+            .ok_or(AoCError::Solving)?;
+
+        Ok(format!("{},{}", x, y))
     }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let map: Vec<Vec<_>> = (1..=SIZE as i32)
+            .map(|x| {
+                (1..=SIZE as i32)
+                    .map(move |y| {
+                        // Find the fuel cell's rack ID,
+                        // which is its X coordinate plus 10.
+                        let rack_id = x + 10;
+                        // Begin with a power level of the rack ID times the Y coordinate.
+                        let mut power_level = rack_id * y;
+                        // Increase the power level by the value of the grid serial number (your puzzle input).
+                        power_level += self.0;
+                        // Set the power level to itself multiplied by the rack ID.
+                        power_level *= rack_id;
+                        // Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+                        power_level /= 100;
+                        power_level %= 10;
+                        // Subtract 5 from the power level.
+                        power_level - 5
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let (x, y, size) = (1..=SIZE)
+            .flat_map(|size| {
+                (1..=SIZE + 1 - size)
+                    .flat_map(move |x| (1..=SIZE + 1 - size).map(move |y| (x, y, size)))
+            })
+            .max_by_key(|&(x, y, size)| {
+                (0..size)
+                    .map(|dx| {
+                        (0..size)
+                            .map(|dy| map[x - 1 + dx][y - 1 + dy])
+                            .sum::<i32>()
+                    })
+                    .sum::<i32>()
+            })
+            .ok_or(AoCError::Solving)?;
+
+        Ok(format!("{},{},{}", x, y, size))
+    }
+
+    fn solve(self) -> AoCResult<aoc_core::Solution>
+    where
+        Self: Sized,
+    {
+        let map: Vec<Vec<_>> = (1..=SIZE as i32)
+            .map(|x| {
+                (1..=SIZE as i32)
+                    .map(move |y| {
+                        // Find the fuel cell's rack ID,
+                        // which is its X coordinate plus 10.
+                        let rack_id = x + 10;
+                        // Begin with a power level of the rack ID times the Y coordinate.
+                        let mut power_level = rack_id * y;
+                        // Increase the power level by the value of the grid serial number (your puzzle input).
+                        power_level += self.0;
+                        // Set the power level to itself multiplied by the rack ID.
+                        power_level *= rack_id;
+                        // Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+                        power_level /= 100;
+                        power_level %= 10;
+                        // Subtract 5 from the power level.
+                        power_level - 5
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let (x, y) = (1..=SIZE - 2)
+            .cartesian_product(1..=SIZE - 2)
+            .max_by_key(|(x, y)| {
+                (0..3)
+                    .map(|dx| {
+                        (0..3)
+                            .map(|dy| map[x - 1 + dx][y - 1 + dy])
+                            .sum::<i32>()
+                    })
+                    .sum::<i32>()
+            })
+            .ok_or(AoCError::Solving)?;
+
+        let part1 = format!("{},{}", x, y);
+
+        let (x, y, size) = (1..=SIZE)
+            .flat_map(|size| {
+                (1..=SIZE + 1 - size)
+                    .flat_map(move |x| (1..=SIZE + 1 - size).map(move |y| (x, y, size)))
+            })
+            .max_by_key(|&(x, y, size)| {
+                (0..size)
+                    .map(|dx| {
+                        (0..size)
+                            .map(|dy| map[x - 1 + dx][y - 1 + dy])
+                            .sum::<i32>()
+                    })
+                    .sum::<i32>()
+            })
+            .ok_or(AoCError::Solving)?;
+
+        let part2 = format!("{},{},{}", x, y, size);
+
+        Ok(Solution {
+            part1: Box::new(part1),
+            part2: Box::new(part2),
+        })
     }
 }
 
@@ -25,17 +172,26 @@ mod test {
 
     #[test]
     fn part_1() {
-        let input = "";
+        let input = "42";
         let data = Data::try_new(input).unwrap();
         let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
+        assert_eq!(result, "21,61");
     }
 
     #[test]
     fn part_2() {
-        let input = "";
+        let input = "42";
         let data = Data::try_new(input).unwrap();
         let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        assert_eq!(result, "232,251,12");
+    }
+
+    #[test]
+    fn solve() {
+        let input = "42";
+        let data = Data::try_new(input).unwrap();
+        let Solution { part1, part2 } = data.solve().unwrap();
+        assert_eq!(part1.to_string(), "21,61");
+        assert_eq!(part2.to_string(), "90,269,16");
     }
 }
--- advent_of_code/2018/solutions/src/day_12.rs
+++ advent_of_code/2018/solutions/src/day_12.rs
@@ -1,21 +1,139 @@
 use std::fmt::Display;
 
+use aoc_core::AoCError;
+
 use crate::{AoCData, AoCResult};
 
 #[derive(Debug, Clone)]
-pub struct Data;
+pub struct Data<'a>(&'a str);
 
-impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+struct Tunnel {
+    current_gen: std::vec::Vec<bool>,
+    next_gen: std::vec::Vec<bool>,
+    offset: usize,
+    evolutions: [bool; 32],
+}
+
+impl Tunnel {
+    fn parse(input_string: &str, space_for_generations: usize) -> Result<Self, AoCError> {
+        let mut evolutions = [false; 32];
+
+        let mut lines = input_string.lines();
+        let next_line = lines.next().ok_or(AoCError::Parsing)?;
+        let prefix_length = "initial state: ".len();
+
+        let initial_line: &str = next_line
+            .get(prefix_length..)
+            .ok_or(AoCError::Parsing)?;
+
+        let max_growth = space_for_generations * 2;
+        let state_length = initial_line.len() + 2 * max_growth;
+        let mut current_gen = vec![false; state_length];
+        let next_gen = vec![false; state_length];
+        for (i, byte) in initial_line.bytes().enumerate() {
+            current_gen[max_growth + i] = byte == b'#';
+        }
+
+        lines.next(); // Skip empty line
+        for line in lines {
+            let (part1, part2) = line
+                .split_once(" => ")
+                .ok_or_else(|| AoCError::Parsing)?;
+            if part2 == "#" {
+                let from_bytes: Vec<u8> = part1.bytes().collect();
+                if from_bytes.len() != 5 {
+                    return Err(AoCError::Parsing);
+                }
+                let from = (usize::from(from_bytes[0] == b'#'))
+                    + ((usize::from(from_bytes[1] == b'#')) << 1)
+                    + ((usize::from(from_bytes[2] == b'#')) << 2)
+                    + ((usize::from(from_bytes[3] == b'#')) << 3)
+                    + ((usize::from(from_bytes[4] == b'#')) << 4);
+                evolutions[from] = true;
+            }
+        }
+
+        Ok(Self {
+            current_gen,
+            next_gen,
+            offset: max_growth,
+            evolutions,
+        })
+    }
+
+    fn evolve(&mut self) {
+        for i in 2..self.current_gen.len() - 2 {
+            let current = usize::from(self.current_gen[i - 2])
+                + (usize::from(self.current_gen[i - 1]) << 1)
+                + (usize::from(self.current_gen[i]) << 2)
+                + (usize::from(self.current_gen[i + 1]) << 3)
+                + (usize::from(self.current_gen[i + 2]) << 4);
+            self.next_gen[i] = self.evolutions[current];
+        }
+
+        std::mem::swap(&mut self.next_gen, &mut self.current_gen);
+    }
+
+    fn is_repeating(&self) -> bool {
+        self.current_gen
+            .iter()
+            .skip_while(|&&populated| !populated)
+            .zip(
+                self.next_gen
+                    .iter()
+                    .skip_while(|&&populated| !populated),
+            )
+            .all(|(a, b)| a == b)
+    }
+
+    fn score(&self) -> i64 {
+        let mut sum = 0;
+        for (index, &value) in self.current_gen.iter().enumerate() {
+            if value {
+                let index = index as i32 - self.offset as i32;
+                sum += i64::from(index);
+            }
+        }
+        sum
+    }
+}
+
+impl<'a> AoCData<'a> for Data<'a> {
+    fn try_new(input: &'a str) -> AoCResult<Self> {
+        Ok(Self(input))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let max_steps = 20;
+
+        let mut tunnel = Tunnel::parse(self.0, max_steps)?;
+
+        for _ in 0..20 {
+            tunnel.evolve();
+        }
+
+        Ok(tunnel.score())
     }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let max_steps = 50;
+
+        let mut tunnel = Tunnel::parse(self.0, max_steps)?;
+
+        let mut previous_score = -1;
+        for generation in 1.. {
+            tunnel.evolve();
+
+            let score_diff = tunnel.score() - previous_score;
+            previous_score = tunnel.score();
+
+            if tunnel.is_repeating() {
+                let remaining_generations = 50_000_000_000_i64 - generation as i64;
+                let final_score = tunnel.score() + remaining_generations * score_diff;
+                return Ok(final_score);
+            }
+        }
+        Err(AoCError::Solving)
     }
 }
 
@@ -25,17 +143,24 @@ mod test {
 
     #[test]
     fn part_1() {
-        let input = "";
+        let input = "initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #";
         let data = Data::try_new(input).unwrap();
         let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
-    }
-
-    #[test]
-    fn part_2() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        assert_eq!(result, "325");
     }
 }
