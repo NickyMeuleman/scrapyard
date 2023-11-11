b35cfb09f621f14310c3f26468635c0fc5b6e021
Nicky Meuleman <nicky.dev@outlook.com>
2023-11-11 20:45:59 +0100

    aoc2019 day08

--- advent_of_code/2019/solutions/src/day_08.rs
+++ advent_of_code/2019/solutions/src/day_08.rs
@@ -1,41 +1,80 @@
 use std::fmt::Display;
 
+use aoc_core::AoCError;
+
 use crate::{AoCData, AoCResult};
 
-#[derive(Debug, Clone)]
-pub struct Data;
+const WIDTH: usize = 25;
+const HEIGHT: usize = 6;
+const SIZE: usize = WIDTH * HEIGHT;
 
-impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+#[derive(Debug, Clone)]
+pub struct Data<'a>(&'a str);
+
+impl<'a> AoCData<'a> for Data<'a> {
+    fn try_new(input: &'a str) -> AoCResult<Self> {
+        Ok(Self(input))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        self.0
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .array_chunks::<SIZE>()
+            .min_by(|chunk_a, chunk_b| {
+                let zeroes_a = chunk_a
+                    .iter()
+                    .filter(|n| **n == 0)
+                    .count();
+                let zeroes_b = chunk_b
+                    .iter()
+                    .filter(|n| **n == 0)
+                    .count();
+                zeroes_a.cmp(&zeroes_b)
+            })
+            .map(|chunk| {
+                let ones = chunk
+                    .iter()
+                    .filter(|n| **n == 1)
+                    .count();
+                let twos = chunk
+                    .iter()
+                    .filter(|n| **n == 2)
+                    .count();
+                ones * twos
+            })
+            .ok_or(AoCError::Solving)
     }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
-    }
-}
+        let mut image = [2u32; SIZE];
+        for chunk in self
+            .0
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .array_chunks::<SIZE>()
+        {
+            for (idx, color) in chunk.iter().enumerate() {
+                if image[idx] != 2 {
+                    continue;
+                }
+                image[idx] = *color;
+            }
+        }
 
-#[cfg(test)]
-mod test {
-    use super::*;
+        let mut result = String::new();
+        for height in 0..HEIGHT {
+            for width in 0..WIDTH {
+                let color = image[height * WIDTH + width];
+                match color {
+                    0 => result.push(' '),
+                    1 => result.push('■'),
+                    _ => return Err(AoCError::Solving),
+                }
+            }
+            result.push('\n');
+        }
 
-    #[test]
-    fn part_1() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
-    }
-
-    #[test]
-    fn part_2() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        Ok(result)
     }
 }
