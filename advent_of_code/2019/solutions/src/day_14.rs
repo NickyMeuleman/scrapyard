3eb53309b899236c1c1e6a9faeeab7e3214f038e
Nicky Meuleman <nicky.dev@outlook.com>
2023-11-20 22:23:30 +0100

    aoc2019 day14

--- advent_of_code/2019/solutions/src/day_14.rs
+++ advent_of_code/2019/solutions/src/day_14.rs
@@ -1,21 +1,129 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
+
+use aoc_core::{AoCError, Solution};
 
 use crate::{AoCData, AoCResult};
 
 #[derive(Debug, Clone)]
-pub struct Data;
+pub struct Data<'a>(HashMap<&'a str, Recipe<'a>>);
 
-impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
+impl<'a> Data<'a> {
+    fn ore_for_fuel(&self, target: u64) -> u64 {
+        let mut needed: Vec<(u64, &str)> = vec![(target, "FUEL")];
+        let mut storage: HashMap<&str, u64> = HashMap::new();
+        let mut ore_needed = 0;
+
+        while let Some((mut needed_count, needed_name)) = needed.pop() {
+            if needed_name == "ORE" {
+                ore_needed += needed_count;
+                continue;
+            }
+
+            let recipe = &self.0.get(&needed_name).unwrap();
+
+            // use stored reactants if possible, only take the needed amount from storage
+            if let Some(stored) = storage.get_mut(needed_name) {
+                let taken_from_storage = needed_count.min(*stored);
+                *stored -= taken_from_storage;
+                needed_count -= taken_from_storage;
+            }
+
+            let needed_reactions_count = needed_count.div_ceil(recipe.count as u64);
+            let made = needed_reactions_count * recipe.count as u64;
+            let surplus = made.saturating_sub(needed_count);
+
+            storage
+                .entry(needed_name)
+                .and_modify(|count| *count += surplus)
+                .or_insert(surplus);
+
+            for (reactant_count, reactant_name) in &recipe.reactants {
+                needed.push((
+                    *reactant_count as u64 * needed_reactions_count as u64,
+                    reactant_name,
+                ));
+            }
+        }
+
+        ore_needed
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Recipe<'a> {
+    count: u32,
+    reactants: Vec<(u32, &'a str)>,
+}
+
+fn parse_reactant(s: &str) -> AoCResult<(u32, &str)> {
+    let (count, name) = s
+        .split_once(" ")
+        .ok_or(AoCError::Parsing)?;
+    Ok((count.parse()?, name))
+}
+
+impl<'a> AoCData<'a> for Data<'a> {
+    fn try_new(input: &'a str) -> AoCResult<Self> {
+        let mut map = HashMap::new();
+        for line in input.lines() {
+            let (left, right) = line.split_once(" => ").unwrap();
+            let mut reactants = Vec::new();
+            for reactant in left.split_terminator(", ") {
+                reactants.push(parse_reactant(reactant)?);
+            }
+            let (count, product) = parse_reactant(right)?;
+            map.insert(product, Recipe { count, reactants });
+        }
+        Ok(Self(map))
     }
 
     fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
+        let result = self.ore_for_fuel(1);
+        Ok(result)
     }
 
     fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
+        const ORE_AVAILABLE: u64 = 1_000_000_000_000;
+        let mut lower = ORE_AVAILABLE / self.ore_for_fuel(1);
+        let mut higher = ORE_AVAILABLE;
+        loop {
+            if lower + 1 == higher {
+                return Ok(lower);
+            }
+            let mid = (lower + higher) / 2;
+            let ore_needed = self.ore_for_fuel(mid);
+            match ore_needed.cmp(&ORE_AVAILABLE) {
+                std::cmp::Ordering::Less => lower = mid,
+                std::cmp::Ordering::Equal => return Ok(mid),
+                std::cmp::Ordering::Greater => higher = mid,
+            }
+        }
+    }
+
+    fn solve(self) -> AoCResult<Solution>
+    where
+        Self: Sized,
+    {
+        const ORE_AVAILABLE: u64 = 1_000_000_000_000;
+        let ore_for_one = self.ore_for_fuel(1);
+        let mut lower = ORE_AVAILABLE / ore_for_one;
+        let mut higher = ORE_AVAILABLE;
+        loop {
+            if lower + 1 == higher {
+                break;
+            }
+            let mid = (lower + higher) / 2;
+            let ore_needed = self.ore_for_fuel(mid);
+            match ore_needed.cmp(&ORE_AVAILABLE) {
+                std::cmp::Ordering::Less => lower = mid,
+                std::cmp::Ordering::Equal => break,
+                std::cmp::Ordering::Greater => higher = mid,
+            }
+        }
+        return Ok(Solution {
+            part1: Box::new(ore_for_one),
+            part2: Box::new(lower),
+        });
     }
 }
 
@@ -25,17 +133,163 @@ mod test {
 
     #[test]
     fn part_1() {
-        let input = "";
+        let input = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
         let data = Data::try_new(input).unwrap();
         let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
+        assert_eq!(result, "13312");
+
+        let input = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_1().unwrap().to_string();
+        assert_eq!(result, "180697");
+
+        let input = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_1().unwrap().to_string();
+        assert_eq!(result, "2210736");
     }
 
     #[test]
     fn part_2() {
-        let input = "";
+        let input = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
         let data = Data::try_new(input).unwrap();
         let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+        assert_eq!(result, "82892753");
+
+        let input = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_2().unwrap().to_string();
+        assert_eq!(result, "5586022");
+
+        let input = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_2().unwrap().to_string();
+        assert_eq!(result, "460664");
+    }
+
+    #[test]
+    fn solve() {
+        let input = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
+        let data = Data::try_new(input).unwrap();
+        let Solution { part1, part2 } = data.solve().unwrap();
+        assert_eq!(part1.to_string(), "13312");
+        assert_eq!(part2.to_string(), "82892753");
+
+        let input = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+        let data = Data::try_new(input).unwrap();
+        let Solution { part1, part2 } = data.solve().unwrap();
+        assert_eq!(part1.to_string(), "180697");
+        assert_eq!(part2.to_string(), "5586022");
+
+        let input = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
+        let data = Data::try_new(input).unwrap();
+        let Solution { part1, part2 } = data.solve().unwrap();
+        assert_eq!(part1.to_string(), "2210736");
+        assert_eq!(part2.to_string(), "460664");
     }
 }
