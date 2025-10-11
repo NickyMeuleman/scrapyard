a04d8bf0517f4e0a3c0ba3ac74c87fed74daffcd
Nicky Meuleman <nicky.dev@outlook.com>
2025-10-11 17:52:35 +0200

    aoc2019 day19

--- advent_of_code/2019/solutions/src/day_19.rs
+++ advent_of_code/2019/solutions/src/day_19.rs
@@ -1,41 +1,63 @@
+use crate::{intcode::Computer, AoCData, AoCError, AoCResult};
 use std::fmt::Display;
 
-use crate::{AoCData, AoCResult};
-
 #[derive(Debug, Clone)]
-pub struct Data;
+pub struct Data(Vec<i64>);
 
-impl AoCData<'_> for Data {
-    fn try_new(_input: &str) -> AoCResult<Self> {
-        Ok(Self)
-    }
+const SHIP_SIZE: i64 = 100;
 
-    fn part_1(&self) -> AoCResult<impl Display> {
-        Ok("")
-    }
-
-    fn part_2(&self) -> AoCResult<impl Display> {
-        Ok("")
+fn is_pulled(memory: &[i64], x: i64, y: i64) -> AoCResult<bool> {
+    let mut computer: Computer = Default::default();
+    computer.set_memory(memory.to_vec());
+    computer.input(x);
+    computer.input(y);
+    computer.run()?;
+    match computer.consume_output() {
+        Some(1) => Ok(true),
+        Some(0) => Ok(false),
+        _ => Err(AoCError::Solving),
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn part_1() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_1().unwrap().to_string();
-        assert_eq!(result, "");
+impl AoCData<'_> for Data {
+    fn try_new(input: &str) -> AoCResult<Self> {
+        Ok(Self(
+            input
+                .split(',')
+                .filter_map(|s| s.parse().ok())
+                .collect(),
+        ))
     }
 
-    #[test]
-    fn part_2() {
-        let input = "";
-        let data = Data::try_new(input).unwrap();
-        let result = data.part_2().unwrap().to_string();
-        assert_eq!(result, "");
+    fn part_1(&self) -> AoCResult<impl Display> {
+        let mut sum = 0;
+        for x in 0..50 {
+            for y in 0..50 {
+                if is_pulled(&self.0, x, y)? {
+                    sum += 1;
+                }
+            }
+        }
+        Ok(sum)
+    }
+
+    fn part_2(&self) -> AoCResult<impl Display> {
+        // Don't rotate the square; it should be aligned to the same axes as the drone grid
+        // find deph at which beam is 100 wide, then go 100 higher
+        // reason: beam goes to the right, becomes wider as is goes deeper
+        // incrementing top works too, but is less efficient because it requires more checks
+        let mut left = 0;
+        let mut bottom = SHIP_SIZE - 1;
+        loop {
+            bottom += 1;
+            while !is_pulled(&self.0, left, bottom)? {
+                left += 1;
+            }
+            let right = left + (SHIP_SIZE - 1);
+            let top = bottom - (SHIP_SIZE - 1);
+            if is_pulled(&self.0, right, top)? {
+                return Ok(left * 10_000 + top);
+            }
+        }
     }
 }
