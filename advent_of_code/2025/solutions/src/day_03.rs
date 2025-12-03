aa51efe457709d5bb4ddad9b98436216949d26e3
Nicky Meuleman <nicky.dev@outlook.com>
2025-12-03 20:43:11 +0100

    aoc2025 day03

---
+++ advent_of_code/2025/solutions/src/day_03.rs
@@ -0,0 +1,87 @@
+// Blog writeup with simpler Rust code (I should handle errors here):
+// https://nickymeuleman.netlify.app/blog/aoc2025-day03/
+
+use crate::{AoCData, AoCResult};
+use std::fmt::Display;
+
+#[derive(Debug, Clone)]
+pub struct Data<'a>(&'a str);
+
+impl<'a> AoCData<'a> for Data<'a> {
+    fn try_new(input: &'a str) -> AoCResult<Self> {
+        Ok(Self(input))
+    }
+
+    fn part_1(&self) -> AoCResult<impl Display> {
+        Ok(self
+            .0
+            .lines()
+            .filter_map(|line| {
+                let digits: Vec<u8> = line.bytes().map(|b| b - b'0').collect();
+
+                let possibilities_first = &digits[0..(digits.len() - 1)];
+                let first = possibilities_first.iter().max()?;
+                let idx = possibilities_first
+                    .iter()
+                    .position(|d| d == first)?;
+
+                let possibilities_second = &digits[idx + 1..digits.len()];
+                let second = possibilities_second.iter().max()?;
+
+                Some(u32::from(first * 10 + second))
+            })
+            .sum::<u32>())
+    }
+
+    fn part_2(&self) -> AoCResult<impl Display> {
+        Ok(self
+            .0
+            .lines()
+            .filter_map(|line| {
+                let digits: Vec<u8> = line.bytes().map(|b| b - b'0').collect();
+                let mut result = 0;
+                let mut start = 0;
+                for reserved in (0..12).rev() {
+                    // first index we cannot pick
+                    let end = digits.len() - reserved;
+                    let possibilities = &digits[start..end];
+                    let max = possibilities.iter().copied().max()?;
+                    // Max digit can appear more than once, pick the leftmost one to keep more options for next digit.
+                    let idx = possibilities
+                        .iter()
+                        .position(|&d| d == max)?;
+                    result = result * 10 + max as u64;
+                    start += idx + 1;
+                }
+                Some(result)
+            })
+            .sum::<u64>())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_1().unwrap().to_string();
+        assert_eq!(result, "357");
+    }
+
+    #[test]
+    fn part_2() {
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        let data = Data::try_new(input).unwrap();
+        let result = data.part_2().unwrap().to_string();
+        assert_eq!(result, "3121910778619");
+    }
+}
