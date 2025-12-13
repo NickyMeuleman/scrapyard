d66830fd91a4265c8dab8ac3ed461a00352a7339
Nicky Meuleman <nicky.dev@outlook.com>
2025-12-13 17:09:31 +0100

    aoc2025 day1 cleanup

--- advent_of_code/2025/solutions/src/day_01.rs
+++ advent_of_code/2025/solutions/src/day_01.rs
@@ -1,11 +1,30 @@
 // Blog writeup with simpler Rust code (I should handle errors here):
 // https://nickymeuleman.netlify.app/blog/aoc2025-day01/
 
+use aoc_core::AoCError;
+
 use crate::{AoCData, AoCResult};
 use std::fmt::Display;
 
 #[derive(Debug, Clone)]
 pub struct Data<'a>(&'a str);
+
+fn parse_instruction(s: &str) -> AoCResult<(i32, i32)> {
+    let mut chars = s.chars();
+    let dir = match chars.next() {
+        Some('L') => -1,
+        Some('R') => 1,
+        _ => return Err(AoCError::Parsing),
+    };
+    let num = chars.try_fold(0, |acc, c| -> AoCResult<i32> {
+        let digit = c
+            .to_digit(10)
+            .ok_or(AoCError::Parsing)?;
+        Ok(acc * 10 + digit as i32)
+    })?;
+
+    Ok((dir, num))
+}
 
 impl<'a> AoCData<'a> for Data<'a> {
     fn try_new(input: &'a str) -> AoCResult<Self> {
@@ -16,8 +35,7 @@ impl<'a> AoCData<'a> for Data<'a> {
         let mut pos: i32 = 50;
         let mut sum = 0;
         for line in self.0.lines() {
-            let dir = if line.starts_with('R') { 1 } else { -1 };
-            let num: i32 = line[1..].parse()?;
+            let (dir, num) = parse_instruction(line)?;
             pos = (pos + dir * num).rem_euclid(100);
             if pos == 0 {
                 sum += 1
@@ -30,8 +48,7 @@ impl<'a> AoCData<'a> for Data<'a> {
         let mut pos: i32 = 50;
         let mut sum = 0;
         for line in self.0.lines() {
-            let dir = if line.starts_with('R') { 1 } else { -1 };
-            let num: i32 = line[1..].parse()?;
+            let (dir, num) = parse_instruction(line)?;
             for _ in 0..num {
                 pos = (pos + dir).rem_euclid(100);
                 if pos == 0 {
